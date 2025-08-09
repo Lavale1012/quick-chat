2787a690225428d2ed1d1ae303336db509d89b17
Lavale1012 <lavale889@gmail.com>
2025-08-09 18:10:09 -0400

    feat: initialize chat application with basic structure and components
    - Added favicon and logo images for the application.
    - Created index.html as the main entry point for the web app.
    - Implemented manifest.json for PWA support.
    - Added robots.txt for search engine optimization.
    - Developed DarkModeToggle component for theme switching.
    - Created FileInputButton component for file uploads.
    - Set up Tailwind CSS for styling in index.css.
    - Established the main application entry point in index.js.
    - Included performance reporting with reportWebVitals.js.
    - Configured testing setup with jest-dom.
    - Set up Tailwind CSS configuration in tailwind.config.js.
    - Added Vercel configuration for deployment.

---
+++ chatapp/src/components/FileInputButton.js
@@ -0,0 +1,27 @@
+import React, { useRef } from "react";
+import { GrGallery } from "react-icons/gr";
+const FileInputButton = ({ onFileSelect, selectedFile }) => {
+  const fileInputRef = useRef(null);
+
+  const handleButtonClick = () => {
+    fileInputRef.current.click();
+  };
+
+  return (
+    <>
+      <input
+        ref={fileInputRef}
+        type="file"
+        onChange={onFileSelect}
+        className="hidden"
+      />
+      <button
+        onClick={handleButtonClick}
+        className="  p-3 bg-blue-600 text-white rounded hover:bg-blue-700 transition duration-300 active:bg-blue-500"
+      >
+        <GrGallery />
+      </button>
+    </>
+  );
+};
+export default FileInputButton;
